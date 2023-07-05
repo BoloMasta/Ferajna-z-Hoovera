7adbe287d5f5c08059504f8064e08bbf5d778771
BoloMasta <103572991+BoloMasta@users.noreply.github.com>
2023-07-05 17:40:16 +0200

    members fix

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import members from "../../assets/images/members/members";
 import styles from "./Members.module.scss";
 
 const Members = () => {
+  const [activeMember, setActiveMember] = useState(0);
+
   return (
     <section className={styles.members}>
-      <h2 className={styles.title}>Skład zespołu</h2>
-
       <div className={styles.container}>
         <div className={styles.membersImages}>
-          {members.members.map((member, index) => (
-            <div className={styles.member} key={index}>
+          {members.members.map((member) => (
+            <div
+              className={styles.member}
+              key={member.id}
+              onClick={() => setActiveMember(member.id)}
+            >
               <img src={member.avatar} alt={member.avatarAlt} className={styles.memberAvatar} />
               <img src={member.photo} alt={member.photoAlt} className={styles.memberPhoto} />
             </div>
@@ -17,9 +22,26 @@ const Members = () => {
         </div>
 
         <div className={styles.membersText}>
-          <p className={styles.text}>
-            Zespół Ferajna z Hoovera powstał w 2019 roku. W skład zespołu wchodzą: <br />
+          <p className={styles.header}>
+            Imię:{" "}
+            <span className={styles.text}>
+              {activeMember ? members.members[activeMember - 1].name : ""}
+            </span>
           </p>
+          <p className={styles.header}>
+            Pseudonim:{" "}
+            <span className={styles.text}>
+              {activeMember ? `"` + members.members[activeMember - 1].alias + `"` : ""}
+            </span>
+          </p>
+          {activeMember ? (
+            <div
+              className={styles.text}
+              dangerouslySetInnerHTML={{ __html: members.members[activeMember - 1].description }}
+            ></div>
+          ) : (
+            <div className={styles.text}>Wybierz członka zespołu</div>
+          )}
         </div>
       </div>
     </section>
