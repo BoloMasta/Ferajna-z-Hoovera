859a718e62fa7c0188e5194efd0a9441b9bdeb5d
BoloMasta <103572991+BoloMasta@users.noreply.github.com>
2023-07-06 17:41:18 +0200

    teaser

--- src/components/Teaser/Teaser.jsx
+++ src/components/Teaser/Teaser.jsx
@@ -10,7 +10,7 @@ const Teaser = () => {
           <h2 className={styles.title}>Kto się tak wydziera?</h2>
         </Parallax>
 
-        <Parallax
+        {/* <Parallax
           translateX={["-30px", "600px"]}
           translateY={["0px", "-40px"]}
           scale={[1, 0.75]}
@@ -19,34 +19,49 @@ const Teaser = () => {
           opacity={[1, 0]}
         >
           <img src="../images/loader.png" alt="Ferajna z Hoovera" className={styles.logo} loading="lazy" />
-        </Parallax>
+        </Parallax> */}
 
-        <Parallax speed={-50} translateY={[50, 0]}>
+        <Parallax speed={-50} translateY={[70, 0]} opacity={[0.5, 1]}>
           <p className={styles.text}>
-            Witamy na stronie naszego zespołu. Znajdziesz tu informacje o nas, naszej twórczości oraz o tym jak się z
-            nami skontaktować. Zapraszamy do zapoznania się z naszymi wykonaniami oraz do kontaktu w sprawie
-            ewentualnych propozycji koncertowych.
+            Witamy na stronie naszego zespołu. Znajdziesz tu informacje o nas, naszej twórczości
+            oraz o tym jak się z nami skontaktować. Zapraszamy do zapoznania się z naszymi
+            wykonaniami oraz do kontaktu w sprawie ewentualnych propozycji koncertowych.
           </p>
         </Parallax>
 
         <div className={styles.grid}>
           <div className={styles.gridItem}>
             <Link to="/about" onClick={() => window.scrollTo(0, 0)}>
-              <img src="../images/teaser1.jpg" alt="Ferajna z Hoovera" className={styles.image} loading="lazy" />
+              <img
+                src="../images/teaser1.jpg"
+                alt="Ferajna z Hoovera"
+                className={styles.image}
+                loading="lazy"
+              />
               <p className={styles.description}>Dowiedz się kim jesteśmy</p>
             </Link>
           </div>
 
           <div className={styles.gridItem}>
             <Link to="/music" onClick={() => window.scrollTo(0, 0)}>
-              <img src="../images/teaser2.jpg" alt="Ferajna z Hoovera" className={styles.image} loading="lazy" />
+              <img
+                src="../images/teaser2.jpg"
+                alt="Ferajna z Hoovera"
+                className={styles.image}
+                loading="lazy"
+              />
               <p className={styles.description}>Posłuchaj co gramy</p>
             </Link>
           </div>
 
           <div className={styles.gridItem}>
             <Link to="/contact" onClick={() => window.scrollTo(0, 0)}>
-              <img src="../images/teaser3.jpg" alt="Ferajna z Hoovera" className={styles.image} loading="lazy" />
+              <img
+                src="../images/teaser3.jpg"
+                alt="Ferajna z Hoovera"
+                className={styles.image}
+                loading="lazy"
+              />
               <p className={styles.description}>Skontaktuj się z nami</p>
             </Link>
           </div>
