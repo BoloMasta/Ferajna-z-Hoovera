6986a7497a2142123721405c6f626be792636ed5
BoloMasta <boleslawadamiec@gmail.com>
2023-09-27 01:26:38 +0200

    test

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -12,11 +12,18 @@ const Switch = (props) => {
   if (props.active === "left") {
     leftTitle.classList.add(`${styles.titleActive}`);
     rightTitle.classList.remove(`${styles.titleActive}`);
+
+    if (leftImage) leftImage.classList.add(`${styles.imageActive}`);
+    if (rightImage) rightImage.classList.remove(`${styles.imageActive}`);
+
     //leftImage.classList.add(`${styles.imageActive}`);
     //rightImage.classList.remove(`${styles.imageActive}`);
   } else if (props.active === "right") {
     leftTitle.classList.remove(`${styles.titleActive}`);
     rightTitle.classList.add(`${styles.titleActive}`);
+
+    if (leftImage) leftImage.classList.remove(`${styles.imageActive}`);
+    if (rightImage) rightImage.classList.add(`${styles.imageActive}`);
     //rightImage.classList.add(`${styles.imageActive}`);
     //leftImage.classList.remove(`${styles.imageActive}`);
   }
