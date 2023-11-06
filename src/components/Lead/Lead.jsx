41115480743232f162db0ad510ccac6075c9e823
BoloMasta <103572991+BoloMasta@users.noreply.github.com>
2023-11-06 17:05:22 +0100

    Update Lead.jsx

--- src/components/Lead/Lead.jsx
+++ src/components/Lead/Lead.jsx
@@ -1,8 +1,4 @@
 import { Parallax } from "react-scroll-parallax";
-
-//import { ParallaxBanner } from "react-scroll-parallax";
-// import { BannerLayer } from "react-scroll-parallax/dist/components/ParallaxBanner/types";
-
 import styles from "./Lead.module.scss";
 
 const Lead = () => {
@@ -11,37 +7,6 @@ const Lead = () => {
       <Parallax speed={20} translateY={[250, -100]} opacity={[-3, 2]}>
         <h1 className={styles.title}>Serwus</h1>
       </Parallax>
-
-      {/* <ParallaxBanner
-        layers={[
-          {
-            image: "/images/lead.jpg",
-            speed: -130,
-            translateY: [0, 30],
-            style: { filter: "blur(1px)", inset: "-100px 0px 0px 0px" },
-            onProgress: (progress) => console.log(progress),
-          },
-          {
-            speed: -15,
-            opacity: [0.5, 1],
-            translateY: [50, 30],
-
-            children: <h1 className={styles.title}>Serwus</h1>,
-          },
-
-          {
-            image: "/images/lead-front.png",
-            speed: -10,
-          },
-        ]}
-        style={{
-          height: "650px",
-          maxHeight: "650px",
-          width: "100%",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      /> */}
     </section>
   );
 };
